b71b25c026c0566b30ea811588a5176874adadfe
Vladimir <Flin010@inbox.ru>
2022-06-07 23:48:38 +0300

    вывод категорий

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -25,13 +25,60 @@
     <section class="content">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-2">
+                <div class="col-sm-4 col-md-2">
                     <a href="{{ route('admin.category.create') }}" class="btn btn-block btn-primary">Добавить</a>
                 </div>
             </div>
-            <div class="row">
-                <div class="col12">
-                    Основной контет страницы ктегории
+            <div class="row mt-5">
+                <div class="col-md-12 col-xl-10">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Список категорий</h3>
+                        </div>
+                        <div class="card-body">
+                            <table class="table table-bordered">
+                                <thead>
+                                <tr>
+                                    <th style="width: 10px">#</th>
+                                    <th>Категория</th>
+                                    <th>Популярность</th>
+                                    <th style="width: 40px">Управление</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($categories as $category)
+                                    <tr>
+                                        <td>{{ $category->id }}</td>
+                                        <td>{{ $category->title }}</td>
+                                        <td class="align-middle">
+                                            <div class="progress progress-xs">
+                                                <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
+                                            </div>
+                                        </td>
+                                        <td class="text-center">
+                                            <a href="#" class="mr-2">
+                                                <i class="fas fa-edit"></i>
+                                            </a>
+                                            <a href="#" class="text-danger">
+                                                <i class="fas fa-trash"></i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                        <div class="card-footer clearfix">
+                            <ul class="pagination pagination-sm m-0 float-right">
+                                <li class="page-item"><a class="page-link" href="#">«</a></li>
+                                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item"><a class="page-link" href="#">»</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div><!-- /.container-fluid -->
